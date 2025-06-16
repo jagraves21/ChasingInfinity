23f87a9cc734a4c2717decee5f38fa5134779994
Jeffrey Graves <jagraves21@tntech.edu>
2025-06-15 21:29:12 -0500

    added new fractals

---
+++ src/main/java/geometric/fractals/CircularInfinity1.java
@@ -0,0 +1,135 @@
+package geometric.fractals;
+
+import utils.color.NamedColors;
+import utils.color.NamedPalettes;
+
+import renderer.WorldViewer;
+
+import geometric.AbstractGeometricFractal;
+import geometric.Circle;
+import geometric.Point;
+import geometric.Transformable;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Iterator;
+
+import java.awt.Color;
+import java.awt.MultipleGradientPaint;
+import java.awt.Paint;
+import java.awt.RadialGradientPaint;
+
+public class CircularInfinity1 extends AbstractGeometricFractal<CircularInfinity1> {
+	public static final Color[] COLORS = NamedPalettes.GAS_ON_WATER;
+
+	protected Point center;
+	protected Point pointOnCircle;
+	protected List<Circle> seed;
+	protected List<Circle> currentCircles;
+	protected List<Transformable> fractalComponents;
+
+	public CircularInfinity1() {
+		this(getSuggestedIterations());
+	}
+
+	public CircularInfinity1(int iterations) {
+		this(iterations, true);
+	}
+
+	public CircularInfinity1(boolean reset) {
+		this(getSuggestedIterations(), true);
+	}
+
+	public CircularInfinity1(int iterations, boolean reset) {
+		super(iterations, reset);
+	}
+
+	public static int getSuggestedIterations() {
+		return 5;
+	}
+
+	public Paint getPaint() {
+		return getPaint(
+			this.center,
+			this.pointOnCircle
+		);
+	}
+
+	public Paint getPaint(WorldViewer worldViewer) {
+		return getPaint(
+			this.center.toScreen(worldViewer),
+			this.pointOnCircle.toScreen(worldViewer)
+		);
+	}
+
+	protected Paint getPaint(Point center, Point pointOnCircle) {
+		return new RadialGradientPaint(
+			(float) center.getX(),
+			(float) center.getY(),
+			(float) center.distance(pointOnCircle),
+			new float[] {0.0f, 1.0f},
+			new Color[] {NamedColors.BLUE, NamedColors.RED}
+			//MultipleGradientPaint.CycleMethod.REPEAT
+		);
+	}
+
+	protected void init() {
+		super.init();
+
+		seed = new LinkedList<>();
+		Circle circle = new Circle(0, 0, 200, NamedColors.WHITE, false);
+
+		seed.add(circle);
+
+		center = new Point(circle.getCenter());
+		pointOnCircle = new Point(center).translate(0, -circle.getRadius());
+		currentCircles = seed;
+		fractalComponents = new LinkedList<>(seed);
+	}
+
+	public void reset() {
+		super.reset();
+		currentCircles = seed;
+		fractalComponents.clear();
+		fractalComponents.addAll(seed);
+	}
+
+	public void step() {
+		List<Circle> nextCircles = new LinkedList<>();
+		for (Circle circle : currentCircles) {
+			nextCircles.add(
+				new Circle(
+					circle.getCenter(),
+					circle.getRadius()/2,
+					COLORS[this.curIteration % COLORS.length]
+				).translate(-circle.getRadius(), 0)
+			);
+			nextCircles.add(
+				new Circle(
+					circle.getCenter(),
+					circle.getRadius()/2,
+					COLORS[this.curIteration % COLORS.length]
+				).translate(circle.getRadius(), 0)
+			);
+		}
+		fractalComponents.addAll(nextCircles);
+		currentCircles = nextCircles;
+	}
+
+	public CircularInfinity1 self() {
+		return this;
+	}
+
+	public Iterator<Transformable> iterator() {
+		return fractalComponents.iterator();
+	}
+
+	public String toString() {
+		return "Circular Infinity 1";
+	}
+
+	public static void main(String[] args) {
+		AbstractGeometricFractal.display(args);
+	}
+}
+
---
+++ src/main/java/geometric/fractals/CircularInfinity4.java
@@ -0,0 +1,149 @@
+package geometric.fractals;
+
+import utils.color.NamedColors;
+import utils.color.NamedPalettes;
+
+import renderer.WorldViewer;
+
+import geometric.AbstractGeometricFractal;
+import geometric.Circle;
+import geometric.Point;
+import geometric.Transformable;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Iterator;
+
+import java.awt.Color;
+import java.awt.MultipleGradientPaint;
+import java.awt.Paint;
+import java.awt.RadialGradientPaint;
+
+public class CircularInfinity4 extends AbstractGeometricFractal<CircularInfinity4> {
+	public static final Color[] COLORS = NamedPalettes.GAS_ON_WATER;
+
+	protected Point center;
+	protected Point pointOnCircle;
+	protected List<Circle> seed;
+	protected List<Circle> currentCircles;
+	protected List<Transformable> fractalComponents;
+
+	public CircularInfinity4() {
+		this(getSuggestedIterations());
+	}
+
+	public CircularInfinity4(int iterations) {
+		this(iterations, true);
+	}
+
+	public CircularInfinity4(boolean reset) {
+		this(getSuggestedIterations(), true);
+	}
+
+	public CircularInfinity4(int iterations, boolean reset) {
+		super(iterations, reset);
+	}
+
+	public static int getSuggestedIterations() {
+		return 5;
+	}
+
+	public Paint getPaint() {
+		return getPaint(
+			this.center,
+			this.pointOnCircle
+		);
+	}
+
+	public Paint getPaint(WorldViewer worldViewer) {
+		return getPaint(
+			this.center.toScreen(worldViewer),
+			this.pointOnCircle.toScreen(worldViewer)
+		);
+	}
+
+	protected Paint getPaint(Point center, Point pointOnCircle) {
+		return new RadialGradientPaint(
+			(float) center.getX(),
+			(float) center.getY(),
+			(float) center.distance(pointOnCircle),
+			new float[] {0.0f, 1.0f},
+			new Color[] {NamedColors.BLUE, NamedColors.RED}
+			//MultipleGradientPaint.CycleMethod.REPEAT
+		);
+	}
+
+	protected void init() {
+		super.init();
+
+		seed = new LinkedList<>();
+		Circle circle = new Circle(0, 0, 200, NamedColors.WHITE, false);
+
+		seed.add(circle);
+
+		center = new Point(circle.getCenter());
+		pointOnCircle = new Point(center).translate(0, -circle.getRadius());
+		currentCircles = seed;
+		fractalComponents = new LinkedList<>(seed);
+	}
+
+	public void reset() {
+		super.reset();
+		currentCircles = seed;
+		fractalComponents.clear();
+		fractalComponents.addAll(seed);
+	}
+
+	public void step() {
+		List<Circle> nextCircles = new LinkedList<>();
+		for (Circle circle : currentCircles) {
+			nextCircles.add(
+				new Circle(
+					circle.getCenter(),
+					circle.getRadius()/2,
+					COLORS[this.curIteration % COLORS.length]
+				).translate(-circle.getRadius()/2, 0)
+			);
+			nextCircles.add(
+				new Circle(
+					circle.getCenter(),
+					circle.getRadius()/2,
+					COLORS[this.curIteration % COLORS.length]
+				).translate(circle.getRadius()/2, 0)
+			);
+			nextCircles.add(
+				new Circle(
+					circle.getCenter(),
+					circle.getRadius()/2,
+					COLORS[this.curIteration % COLORS.length]
+				).translate(0, -circle.getRadius()/2)
+			);
+			nextCircles.add(
+				new Circle(
+					circle.getCenter(),
+					circle.getRadius()/2,
+					COLORS[this.curIteration % COLORS.length]
+				).translate(0, circle.getRadius()/2)
+			);
+		}
+		fractalComponents.addAll(nextCircles);
+		currentCircles = nextCircles;
+	}
+
+	public CircularInfinity4 self() {
+		return this;
+	}
+
+	public Iterator<Transformable> iterator() {
+		return fractalComponents.iterator();
+	}
+
+	public String toString() {
+		return "Circular Infinity 4";
+	}
+
+	public static void main(String[] args) {
+		AbstractGeometricFractal.display(args);
+	}
+}
+
